be3f7cda860ec051b65439339dba998e4f187910
PCalvoGarcia <paulacalvogarcia@gmail.com>
2024-10-07 12:01:25 +0200

    Ejercicio 15 solucionado error

--- basic.js
+++ basic.js
@@ -164,7 +164,7 @@ console.log(res3)
 let res4 = a == 6 || b == 0 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
 console.log(res4)
 
-let res5 = a == 0 && b == 3 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
+let res5 = a == 0 || b == 3 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
 console.log(res5)
 
 let res6 = a == 6 && b == 3 //Eliminar el espacio "__" y coloca el comparador lógico que consideres para que el resultado sea true
